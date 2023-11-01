48817715586cbcafb752df9e927ff28c0f80b6aa
Ksyer <a593510703@gmail.com>
2023-11-01 09:38:00 +0800

    Change parfor & Add simulaton result

--- main.m
+++ main.m
@@ -3,9 +3,9 @@ M = 100;
 T = 50;
 
 prob = zeros(N, M);
-parfor n = 1: N
-    for s = 1: M
+for n = 1: N
+    parfor s = 1: M
         prob(n, s) = get_recovery_prob(n, s, T);
     end
 end
-heatmap(prob)
+c
